f7f952d246ac254e1ad60735c2a5a182484a388c
abit <abit@itu.dk>
2017-03-19 14:38:13 +0100

    Introduces important util functions.
    The bigrams and the vocabulary of a given article, title found in
    wikipedia dump files, are returned as tuple instance (bigram,
    vocabulary)

--- src/request.py
+++ src/request.py
@@ -1,9 +1,29 @@
 import requests, nltk
 
-def get_content_for(x):
+def get_bigrams_and_vocabulary(article):
+    if _preprocess(article) != '':
+        entry = _get_content_for(article)
+        entry = [w['extract'] for w in entry]
+        entry = reduce(lambda l1, l2: l1 + l2, entry)
+        entry = entry.encode('ascii', 'ignore')
+        entry = entry.strip()
+        tokens = nltk.wordpunct_tokenize(entry)
+        return (nltk.bigrams(tokens), sorted(set(tokens)))
+    return None
+
+def _preprocess(x):
+    if  ' ' in x:
+        return x
+    elif len([w for w in x if w.isupper()]) == 1:
+        return x
+    return "" 
+
+def _get_content_for(x):
     """Credits goes to: 
+        Author: Mark Amery
+        See post for more details:
         http://stackoverflow.com/questions/4452102/how-to-get-plain-text-out-of-wikipedia.
-        Author: Mark Amery"""
+        """
     response = requests.get(
             'https://en.wikipedia.org/w/api.php',
             params={
@@ -16,18 +36,4 @@ def get_content_for(x):
             }
     ).json()
     return (iter(response['query']['pages'].values()))
-
-def preprocess(x):
-    if  ' ' in x:
-        return x
-    elif len([w for w in x if x.isupper]) > 1:
-        return ''
-
-def encode(x):
-    return x.replace("\n", "").encode('ascii', 'ignored')
-
-
-entry = get_content_for('Mathematics');
-paragraph =  next(entry)['extract']
-
 
